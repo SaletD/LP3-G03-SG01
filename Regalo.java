7b17e828cee2d893a3b6ace8646ccdec466bdd21
irbazz <108552371+irbazz@users.noreply.github.com>
2023-08-20 17:38:12 -0500

    Create Regalo.java

---
+++ Regalo.java
@@ -0,0 +1,10 @@
+public class Regalo{
+	public static String elegir(int vida){
+		String gift = "";
+		case 1:gift="Un pasaje al caribe";break;
+		case 2:gift="Un visita al museo mas cercano a tu casa";break;
+		case 3:gift="Una entrada al cine";break;
+		}
+		return gift;
+	}
+}
