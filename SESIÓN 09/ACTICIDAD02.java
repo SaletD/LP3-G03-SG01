bdfbd3d7f148e61d495b2ddbeb96d7d4b7380d5b
SaletD <108552042+SaletD@users.noreply.github.com>
2023-11-11 16:30:59 -0500

    Create ACTICIDAD02.java

---
+++ SESIÓN 09/ACTICIDAD02.java
@@ -0,0 +1,20 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String cadena;
+        try (PrintWriter salida=new PrintWriter ("c:/ficheros/datos.txt")){
+            System.out.println("Introduce texto. Para acabar introduce la candena Fin: ");
+            cadena=sc.nextLine();
+            while (!cadena.equalsIgnoreCase("FIN")){
+                salida.println(cadena);
+                cadena=sc.nextLine();
+            }
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
