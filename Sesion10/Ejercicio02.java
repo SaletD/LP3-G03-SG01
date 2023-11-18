79e44ef21b2e85d2d0bbac5ef609d6bced235393
irbazz <108552371+irbazz@users.noreply.github.com>
2023-11-18 14:15:54 -0500

    Update Ejercicio02.java

--- Sesion10/Ejercicio02.java
+++ Sesion10/Ejercicio02.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Ejer2ses10 {
+public class Ejer02ses10 {
 
     
     public static <T> void intercambiarElementos(T[] array, int indice1, int indice2) {
